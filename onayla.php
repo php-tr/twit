23505760921d7e81217c5127ebb349b2a1e4b303
Adil Ilhan <adilmedya@gmail.com>
2013-03-04 08:41:06 +0200

    if koşulu doğru olsun olmasın aynı işlem yapılıyor
    Koşul doğru olsun olmasın aynı işlem yapıldığından temizlendi.
    
    Yönlendirme client side değil server side yapıldı.

--- onayla.php
+++ onayla.php
@@ -3,13 +3,6 @@ include("config.php");
 $tID = addslashes(trim($_GET['id']));
 $sorgu=mysql_query("UPDATE tweets SET aktive = 1 where tID=".$tID);
 
-if($sorgu)
-{
-	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=admin.php">';
-}
-else
-{
-	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=admin.php">';
-}
+header('Location: admin.php');
 
 ?>
